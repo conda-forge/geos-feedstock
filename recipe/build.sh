b9c6ab188a2a7423db0f8b37eb2a751801d5ecbe
ocefpaf <ocefpaf@gmail.com>
2016-04-05 12:06:35 -0300

    Run make check on OS X [skip appveyor]

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
+if [[ `uname` == 'Darwin' ]]; then
+    export MACOSX_VERSION_MIN="10.7"
+    export CXXFLAGS="-mmacosx-version-min=${MACOSX_VERSION_MIN}"
+    export CXXFLAGS="${CXXFLAGS} -stdlib=libc++ -std=c++11"
+    export LINKFLAGS="-mmacosx-version-min=${MACOSX_VERSION_MIN}"
+    export LINKFLAGS="${LINKFLAGS} -stdlib=libc++ -std=c++11 -L${LIBRARY_PATH}"
+else
+    export CXXFLAGS="-pthread -std=c++11 ${CXXFLAGS}"
+fi
 
-# Problems with cartopy if the -m{32,64} flag is not defined.
+if [[ $(uname) == 'Darwin' ]]; then
+  export LIBRARY_SEARCH_VAR=DYLD_FALLBACK_LIBRARY_PATH
+elif [[ $(uname) == 'Linux' ]]; then
+  export LIBRARY_SEARCH_VAR=LD_LIBRARY_PATH
+fi
+
 # See https://taskman.eionet.europa.eu/issues/14817.
-
 MACHINE_TYPE=`uname -m`
 if [ ${MACHINE_TYPE} == 'x86_64' ]; then
   ARCH="-m64"
@@ -17,10 +30,5 @@ CFLAGS=${ARCH} CPPFLAGS=${ARCH} CXXFLAGS=${ARCH} LDFLAGS=${ARCH} FFLAGS=${ARCH} 
     ./configure --prefix=$PREFIX --without-jni
 
 make
-
-# Failing on OS X: https://travis-ci.org/conda-forge/geos-feedstock/builds/119038524
-if [[ $(uname) == Linux ]]; then
-    make check
-fi
-
+eval ${LIBRARY_SEARCH_VAR}=$PREFIX/lib make check
 make install
