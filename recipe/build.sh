755b7f7c3309b0edd1f50b79e3b0cae0ef0528c2
Filipe Fernandes <ocefpaf@gmail.com>
2017-11-07 12:21:58 -0200

    try opt flag and make check on OS X

--- recipe/build.sh
+++ recipe/build.sh
@@ -18,30 +18,6 @@ fi
 
 ./configure --prefix=$PREFIX
 
-make
-# See to be related to https://trac.osgeo.org/geos/ticket/299
-# Failing on OS X: https://travis-ci.org/conda-forge/geos-feedstock/builds/175667698
-# FAIL: geos_unit
-# ============================================================================
-# Testsuite summary for
-# ============================================================================
-# # TOTAL: 1
-# # PASS:  0
-# # SKIP:  0
-# # XFAIL: 0
-# # FAIL:  1
-# # XPASS: 0
-# # ERROR: 0
-# ============================================================================
-# See tests/unit/test-suite.log
-# ============================================================================
-# make[5]: *** [test-suite.log] Error 1
-# make[4]: *** [check-TESTS] Error 2
-# make[3]: *** [check-am] Error 2
-# make[2]: *** [check-recursive] Error 1
-# make[1]: *** [check-recursive] Error 1
-# make: *** [check] Error 2
-if [[ $(uname) == Linux ]]; then
-    make check
-fi
-make install
+make -j${CPU_COUNT} ${VERBOSE_AT}
+make check -j${CPU_COUNT} ${VERBOSE_AT}
+make install -j${CPU_COUNT} ${VERBOSE_AT}
